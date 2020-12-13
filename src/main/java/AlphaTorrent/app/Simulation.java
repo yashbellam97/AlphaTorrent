01594ee1fcae251b30a8801e0dc955108cdbfc5e
yashbellam97 <yashbellam97@gmail.com>
2020-12-12 19:30:02 -0500

    chokeunchoke

--- src/main/java/AlphaTorrent/app/Simulation.java
+++ src/main/java/AlphaTorrent/app/Simulation.java
@@ -16,15 +16,15 @@ import java.util.*;
 
 public class Simulation {
 
-    public static void simulate(){
+    public static void simulate() {
         Host host = InitializeHost.host;
         Logger.write("Starting simulation...", host.getId());
         while (true) {
             if (!host.isHasFile() && !host.getMissingChunks().isEmpty()) {
                 requestChunks(host);
             }
-////            sendChunks(host);
-//            // System.out.println("Chunks" + host.getChunks().size());
+            //// sendChunks(host);
+            // // System.out.println("Chunks" + host.getChunks().size());
             Logger.write("Chunks" + host.getChunks().size(), host.getId());
             if (!host.isHasFile() && host.getChunks().size() == 67) {
                 host.setHasFile(Boolean.TRUE);
@@ -36,51 +36,64 @@ public class Simulation {
     private static void requestChunks(Host host) {
         List<Neighbour> neighbours = host.getNeighbours();
         Iterator<Neighbour> it = neighbours.iterator();
+        System.out.println("1");
         while (it.hasNext()) {
+            System.out.println("2");
             Neighbour neighbour = it.next();
+
             if (!neighbour.isChoked()) {
-                Optional<Integer> optionalChunk = host.getMissingChunks()
-                        .stream()
-                        .filter(chunkId -> ByteArrayExt.getBit(neighbour.getBitfield(),chunkId))
-                        .findAny();
+                System.out.println("3");
+
+                Optional<Integer> optionalChunk = host.getMissingChunks().stream()
+                        .filter(chunkId -> ByteArrayExt.getBit(neighbour.getBitfield(), chunkId)).findAny();
                 if (optionalChunk.isPresent()) {
+                    System.out.println("4");
+
                     Integer chunkId = optionalChunk.get();
                     host.getMissingChunks().remove(chunkId);
                     host.getRequestedChunks().add(chunkId);
                     ActualMessage message = new ActualMessage();
+                    System.out.println("5");
+
                     message.setType(MessageType.REQUEST);
                     message.setLength(chunkId);
                     message.setSenderId(host.getId());
+                    System.out.println("6");
+
                     Sender.send(neighbour.getHost(), neighbour.getPort(), message);
-                    System.out.println("Piece requested with id: "+message.getLength()+" from: "+ neighbour.getId());
-                    Logger.write("Piece requested with id: "+message.getLength()+" from: "+ neighbour.getId(), host.getId());
+                    System.out
+                            .println("Piece requested with id: " + message.getLength() + " from: " + neighbour.getId());
+                    Logger.write("Piece requested with id: " + message.getLength() + " from: " + neighbour.getId(),
+                            host.getId());
                 }
             }
         }
     }
 
-//    private static void sendChunks(Host host) {
-//        Queue<Task> tasks = TaskManager.tasks;
-//        if (!tasks.isEmpty())
-//            // System.out.println("No of chunks with Manager:"+ tasks.size());
-//            Logger.write("No of chunks with Manager:"+ tasks.size());
-//
-//        while (!tasks.isEmpty()) {
-//            Task task = tasks.remove();
-//            ActualMessage message = new ActualMessage();
-//            message.setType(MessageType.PIECE);
-//            message.setLength(task.getChunkId());
-//            message.setPayload(host.getChunks().get(task.getChunkId()));
-//            message.setSenderId(host.getId());
-//            Neighbour neighbour = host.getNeighbours().
-//                    stream()
-//                    .filter(neigh -> task.getPeerId() == neigh.getId())
-//                    .findFirst()
-//                    .get();
-//            Sender.send(neighbour.getHost(), neighbour.getPort(), message);
-//            // System.out.println("Simulation: Send chunks : Piece sent with id: "+message.getLength()+" to: "+ neighbour.getId());
-//            Logger.write("Simulation: Send chunks : Piece sent with id: "+message.getLength()+" to: "+ neighbour.getId());
-//        }
-//    }
+    // private static void sendChunks(Host host) {
+    // Queue<Task> tasks = TaskManager.tasks;
+    // if (!tasks.isEmpty())
+    // // System.out.println("No of chunks with Manager:"+ tasks.size());
+    // Logger.write("No of chunks with Manager:"+ tasks.size());
+    //
+    // while (!tasks.isEmpty()) {
+    // Task task = tasks.remove();
+    // ActualMessage message = new ActualMessage();
+    // message.setType(MessageType.PIECE);
+    // message.setLength(task.getChunkId());
+    // message.setPayload(host.getChunks().get(task.getChunkId()));
+    // message.setSenderId(host.getId());
+    // Neighbour neighbour = host.getNeighbours().
+    // stream()
+    // .filter(neigh -> task.getPeerId() == neigh.getId())
+    // .findFirst()
+    // .get();
+    // Sender.send(neighbour.getHost(), neighbour.getPort(), message);
+    // // System.out.println("Simulation: Send chunks : Piece sent with id:
+    // "+message.getLength()+" to: "+ neighbour.getId());
+    // Logger.write("Simulation: Send chunks : Piece sent with id:
+    // "+message.getLength()+" to: "+ neighbour.getId());
+    // }
+    // }
 
 }
